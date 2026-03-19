57399a2b43511d4fa7bda2b90c9d410e85811070
Salimata Bagayoko <sbagayoko5@gmail.com>
2026-03-19 10:05:48 +0000

    finance_days

--- models/mart/finance_days.sql
+++ models/mart/finance_days.sql
@@ -4,8 +4,10 @@ SELECT
   ROUND(SUM(revenue), 2) AS revenue,
   ROUND(AVG(revenue), 2) AS average_basket,
   ROUND(SUM(operational_margin), 2) AS operational_margin,
-  ROUND(SUM(purchase_cost), 2) AS purchase_cost
-
+  ROUND(SUM(purchase_cost), 2) AS purchase_cost,
+  ROUND(SUM(shipping_fee), 2) AS shipping_fee,
+  ROUND(SUM(logcost), 2) AS logcost,
+  SUM(quantity) AS total_quantity
 FROM {{ ref('int_orders_operational') }}
 GROUP BY date_date
 ORDER BY date_date DESC
